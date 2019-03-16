d8e4a6cafe0092d0035e20dc45620b2e501dafa7
marudor <marudor@marudor.de>
2019-03-16 12:59:02 +0100

    Change "default" font to Roboto-Mono

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ envsubst < src/config.local.skel > src/config.local.yaml
 
 if [ "$LABELLO_DOWNLOAD_FONT" = "yes" ] || [ "$1" = "download_font" ]; then
     mkdir -p /opt/labello/download_font
-    wget https://github.com/googlefonts/spacemono/raw/master/fonts/SpaceMono-Regular.ttf -O /opt/labello/download_font/Space-Mono.ttf
+    wget https://github.com/google/fonts/raw/master/apache/robotomono/RobotoMono-Regular.ttf -O /opt/labello/download_font/Roboto-Mono.ttf
     echo -e "  - /opt/labello/download_font\n" >> src/config.local.yaml
 fi
 
