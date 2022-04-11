9f4b96127fa667ac4797b7d670d696a13085a16c
telegnom <max@telegnom.org>
2022-04-11 13:42:39 +0200

    drop meinheld as webserver

--- src/labello.py
+++ src/labello.py
@@ -5,13 +5,8 @@ A web-based service designed to print labels on your Brother QL label printer.
 """
 
 from app import app, logger
-import meinheld
 
 
 if __name__ == '__main__':
     logger.info("starting labello webserver")
-    if app.debug:
-        app.run(app.config['host'], app.config['port'])
-    else:
-        meinheld.listen((app.config['host'], app.config['port']))
-        meinheld.run(app)
+    app.run(app.config['host'], app.config['port'])
